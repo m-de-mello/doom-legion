02f879cd2913112c19bb9fc5bf59483ea9041c9d
m-de-mello <murillo.mello@sptech.school>
2024-06-10 21:36:30 -0300

    Adicionando Modelagem e Script do Banco de Dados

---
+++ DATABASE/Script BD - Doom Legion.sql
@@ -0,0 +1,52 @@
+-- CRIANDO A BASE DE DADOS
+CREATE DATABASE DoomLegion;
+USE DoomLegion;
+/*----------------------------------------------------------------------------------------------------------------------------------------*/
+-- CRIANDO AS TABELAS
+
+-- TABELA USUARIO
+CREATE TABLE usuario (
+idUsuario INT PRIMARY KEY AUTO_INCREMENT,
+nome VARCHAR(50) NOT NULL,
+username VARCHAR(30) NOT NULL,
+sexo VARCHAR(15) NOT NULL,
+email VARCHAR(50) NOT NULL,
+senha VARCHAR(25) NOT NULL,
+dataRegistro DATE NOT NULL,
+nivelAcesso VARCHAR(15) NOT NULL,
+CONSTRAINT chkSexo CHECK (sexo IN ('Masculino', 'Feminino', 'Não Informado')),
+CONSTRAINT chkNivelAcesso CHECK (nivelAcesso IN ('Administrador', 'Usuário'))
+);
+
+-- TABELA QUIZ
+CREATE TABLE quiz (
+idQuiz INT PRIMARY KEY AUTO_INCREMENT,
+nome VARCHAR(45) NOT NULL,
+dificuldade VARCHAR(15) NOT NULL,
+maxAcertos INT NOT NULL,
+CONSTRAINT chkDificuldade CHECK (dificuldade IN ('Iniciante', 'Intermediário', 'Avançado'))
+);
+
+-- TABELA TENTATIVA
+CREATE TABLE tentativa (
+idTentativa INT,
+fkUsuario INT,
+fkQuiz INT,
+qtdAcertos INT NOT NULL,
+CONSTRAINT pkTentativa PRIMARY KEY (idTentativa, fkUsuario, fkQuiz),
+CONSTRAINT fkTentativaUsuario FOREIGN KEY (fkUsuario)
+	REFERENCES usuario(idUsuario),
+CONSTRAINT fkTentativaQuiz FOREIGN KEY (fkQuiz)
+	REFERENCES quiz(idQuiz)
+);
+
+-- TABELA POSTAGEM
+CREATE TABLE postagem (
+idPostagem INT,
+fkUsuario INT,
+mensagem VARCHAR(500) NOT NULL,
+dataPostagem DATETIME,
+CONSTRAINT pkPostagem PRIMARY KEY (idPostagem, fkUsuario),
+CONSTRAINT fkPostagemUsuario FOREIGN KEY (fkUsuario)
+	REFERENCES usuario(idUsuario)
+);
